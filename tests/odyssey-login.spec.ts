41cce849485f3b9221db2c94e6a4999ed8e020a1
romain ROGER <romain32.roger@gmail.com>
2023-06-06 17:04:10 +0200

    initial commit

---
+++ tests/odyssey-login.spec.ts
@@ -0,0 +1,6 @@
+// const { test, expect } = require("@playwright/test");
+
+// test("verify 1 + 1 = 2", async ({ page }) => {
+//   await page.goto("https://odyssey.wildcodeschool.com/");
+//   await page.pause();
+// });
